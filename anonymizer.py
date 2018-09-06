2ee08e799fcb36fb215c122d1107c076af617abf
yuta1331 <yua.yoyoyo@gmail.com>
2018-09-07 00:47:18 +0900

    create masking method

--- anonymizer.py
+++ anonymizer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env/ python
+# -*- coding: utf-8 -*-
 
 def quasi_row(row, sensitive):
     if sensitive + 1 < len(row):
@@ -34,14 +35,58 @@ def calc_k(freqlist):
 
 def k_judge(min_k, k): return min_k >= k
 
-def masking(attr, value):
-    return '*'
+def masking(attr, value): # これ以上一般化できない場合はそのままvalueを返す．
+    if attr == 'sex': return value
+    if attr == 'time': return value # 今回は'time'がsensitiveなので付け焼刃コーディング
+
+    mask = value.find('*')
+    if mask == 0: return value # 先頭が既に'*'ならそのまま返す．
+
+    #### 以下属性ごとのマスキング処理 ####
+    if attr == 'tel':
+        if mask == -1: return value[:mask] + '*'
+        else: return value[:mask-1] + '*' + value[mask:]
+
+    elif attr == 'poscode':
+        dash = value.find('-')
+        if mask == -1: return value[:mask] + '*'
+        elif mask == dash + 1: return value[:dash-1] + value[dash:]
+        else: return value[:mask-1] + value[mask:]
+
+    elif attr == 'add0':
+        return u'関東'
+
+    elif attr in ['add1', 'add2']:
+        return '*'
+
+    elif attr == 'add3':
+        rdash = value.rfind('-')
+        if rdash == -1:
+            return '*'
+        else:
+            return value[:rdash]
+
+    elif attr == 'add4':
+        return '*'
+
+    elif attr == 'birth':
+        slash = value.rfind('/')
+        if slash == -1:
+            if mask == -1: return value[:mask] + '*'
+            else: return value[:mask-1] + '*' + value[mask:]
+        else: return value[:slash]
+
+#def datafly(datalist, sensitive, k):
+
 
 if __name__ == '__main__':
     import subset
-    infile = 'hoge.csv'
+    infile = 'original_data.csv'
     sensitive = 9
     k = 3
+    attr_list = ['sex', 'tel',
+             'poscode', 'add0', 'add1', 'add2', 'add3', 'add4',
+             'birth', 'time'] # attributes of infile
     init_row, datalist = subset.parsed_list(infile, sensitive)
 
     ###### frequency ######
@@ -54,4 +99,11 @@ if __name__ == '__main__':
     ##### k-judge #####
     kjudge = k_judge(calc_k, k)
     print('k-satisfied: ', kjudge)
-    ##### #####
+
+    ##### modifying #####
+    n = len(datalist)
+    m = len(datalist[0])
+    for i in range(n):
+        for j in range(m):
+            datalist[i][j] = masking(attr_list[j], datalist[i][j])
+        print(datalist[i])
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import subset
 import anonymizer
@@ -7,7 +8,7 @@ infile = 'original_data.csv'
 outfile = 'anonymized_data.csv'
 
 attr_list = ['sex', 'tel',
-             ['poscode', 'add0', 'add1', 'add2', 'add3', 'add4'],
+             'poscode', 'add0', 'add1', 'add2', 'add3', 'add4',
              'birth', 'time'] # attributes of infile
 sensitive = 9 # sensitive attribute is not modified.
 anonym = 'k-anonymity' # choose the method of anonymization
@@ -16,7 +17,7 @@ k = 3 # for k-anonymity
 ########### initial ############
 init_row, datalist = subset.parsed_list(infile, sensitive)
 
-########### k-anonymity ############
+########### k-anonymize ############
 
 
 ########### output ############
