424705736f7e72015fa5db658f26cc8efc464b70
yuta1331 <yua.yoyoyo@gmail.com>
2018-09-11 16:59:40 +0900

    print calculated k

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ attr_list = ['sex', 'tel',
              'birth', 'time'] # attributes of infile
 sensitive = 9 # sensitive attribute is not modified.
 anonym = 'k-anonymity' # choose the method of anonymization
-k = 3 # for k-anonymity
+k = 4 # for k-anonymity
 
 ########### initial ############
 init_row, datalist = subset.parsed_list(infile, sensitive)
@@ -20,6 +20,10 @@ init_row, datalist = subset.parsed_list(infile, sensitive)
 ########### k-anonymize ############
 datalist = anonymizer.easy_anonymizer(datalist, sensitive, k, attr_list)
 subset.all_sorted_list(datalist, sensitive)
+
+freq = anonymizer.freq_list(datalist, sensitive)
+calc_k = anonymizer.calc_k(freq)
+print('k-anonymity: ', calc_k)
 
 ########### output ############
 subset.csv_composer(init_row, datalist, sensitive, outfile)
