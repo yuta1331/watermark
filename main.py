4aac06148b5b433498421af4b24fdc8ffb180338
yuta1331 <yua.yoyoyo@gmail.com>
2018-09-14 12:13:54 +0900

    prepare watermarking

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import watermark
 
 import collections
 
+
+########### config ############
+
 infile = '../Anonymizer/anonymized_data.csv'
 outfile = 'watermarked_data.csv'
 
@@ -14,18 +17,49 @@ attr_list = ['sex', 'tel',
              'birth', 'time'] # attributes of infile
 
 sensitive = 9
-group_by_attr = ['time']
+group_by_attr = ['time', 'sex']
+
+# 今はrandomな2進数を生成
+water_len = 256
+import random
+water_bin = ''.join([random.choice('01') for i in range(water_len)])
+print(water_bin)
 
 ########### initial ############
 init_row, datalist = subset.parsed_list(infile)
 group_by = [attr_list.index(attr) for attr in group_by_attr]
 
 ########### watermark ############
-datalist = subset.sorted_list(datalist, group_by)
+watermark.watermarker(datalist, group_by, water_bin, attr_list)
 
-attr = [data[9] for data in datalist]
-c = collections.Counter(attr)
-print('minimum group: ', min(c.values()))
+########### check ############
+
+# 最後尾の要素がグループ内の要素数
+# group_collection[i] = [time, sex, num_in_group]
+group_collection = list()
+
+# minを使いたいから
+nums_of_each_group = list()
+
+for data in datalist:
+    if len(group_collection) == 0:
+        group_collection = [[data[i] for i in group_by]]
+        group_collection[0].append(1)
+    else:
+        tmp_list = [data[i] for i in group_by]
+        if tmp_list == group_collection[-1][:-1]:
+            group_collection[-1][-1] += 1
+        else:
+            nums_of_each_group.append(group_collection[-1][-1])
+
+            tmp_list.append(1)
+            group_collection.append(tmp_list)
+
+print('#### group_collection ####')
+for group in group_collection:
+    print(group)
+print('num_of_group:  ', len(group_collection))
+print('minimun group: ', min(nums_of_each_group))
 
 ########### output ############
 subset.csv_composer(init_row, datalist, outfile)
--- watermark.py
+++ watermark.py
@@ -1,5 +1,54 @@
 #! usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def watermarker(datalist, watermark):
+import re
+
+import subset
+
+def address_grouper(attr_list):
+    n = len(attr_list)
+
+    try:
+        first = attr_list.index('addr0')
+    except:
+        for i in range(n):
+            if 'addr' in attr_list[i]:
+                first = i
+                break
+    for i in range(n)[::-1]:
+        if 'addr' in attr_list[i]:
+            last = i
+            return first, last
+
+def watermarker(datalist, group_by, water_bin, attr_list):
+    datalist = subset.sorted_list(datalist, group_by)
+
+    # グループごとに埋め込み
+    tmp_group_key = list()
+    for data in datalist:
+        if len(water_bin) == 0:
+            break
+
+        data_key = [data[i] for i in group_by]
+
+        # 今はグループに1つだけ編集
+        # 住所のみ
+        if len(tmp_group_key) == 0 or tmp_group_key != data_key:
+            tmp_group_key = data_key
+            embeded_bin = water_bin[:2]
+            water_bin = water_bin[2:]
+
+            # 住所を取り出す
+            # 住所はまとめてくっつける
+            addr_first, addr_last = address_grouper(attr_list)
+            address = ''.join(data[addr_first:addr_last+1])
+            address = re.sub(r'\*+', '', address)
+
+            # modifying
+
+            # formatting
+            # address
+
+            # datalist[i][addr_first:addr_last+1] = address
+
     return
