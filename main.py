f120f8375c652462977ff8b9c3036904cdb1496c
yuta1331 <yua.yoyoyo@gmail.com>
2018-12-14 17:41:25 +0900

    fix main.py

--- main.py
+++ main.py
@@ -4,29 +4,35 @@
 import subset
 import anonymizer
 
-infile = 'original_data.csv'
-outfile = 'anonymized_data.csv'
+INFILE = 'original_data.csv'
+OUTFILE = 'anonymized_data.csv'
 
-attr_list = ['sex', 'tel',
+ATTR_LIST = ['sex', 'tel',
              'poscode', 'addr0', 'addr1', 'addr2', 'addr3', 'addr4',
-             'birth', 'time'] # attributes of infile
-sensitive = 9 # sensitive attribute is not modified.
-anonym = 'k-anonymity' # choose the method of anonymization
-k = 4 # for k-anonymity
-attr_priority = ['sex', 'addr0', 'addr1', 'addr2', 'addr3', 'addr4',
+             'birth', 'time']  # attributes of INFILE
+SENSITIVE = 9  # sensitive attribute is not modified.
+ANONYM = 'k-anonymity'  # choose the method of anonymization
+K = 4  # for k-anonymity
+ATTR_PRIORITY = ['sex', 'addr0', 'addr1', 'addr2', 'addr3', 'addr4',
                  'poscode', 'tel', 'birth', 'time']
 
 ########### initial ############
-init_row, datalist = subset.parsed_list(infile, sensitive)
-priority = [attr_list.index(attr) for attr in attr_priority]
+init_row, datalist = subset.parsed_list(INFILE, SENSITIVE)
+priority = [ATTR_LIST.index(attr) for attr in ATTR_PRIORITY]
+
 
 ########### k-anonymize ############
-datalist = anonymizer.easy_anonymizer(datalist, sensitive, k, attr_list, priority)
-subset.all_sorted_list(datalist, sensitive, None)
+datalist = anonymizer.easy_anonymizer(datalist,
+                                      SENSITIVE,
+                                      K,
+                                      ATTR_LIST,
+                                      priority)
+subset.all_sorted_list(datalist, SENSITIVE, None)
 
-freq = anonymizer.freq_list(datalist, sensitive)
+freq = anonymizer.freq_list(datalist, SENSITIVE)
 calc_k = anonymizer.calc_k(freq)
-print('k-anonymity: ', calc_k)
+print(ANONYM, ': ', calc_k)
+
 
 ########### output ############
-subset.csv_composer(init_row, datalist, sensitive, outfile)
+subset.csv_composer(init_row, datalist, SENSITIVE, OUTFILE)
